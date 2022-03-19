e5b9868af0e4b77b6be5d88501a0d42625498637
harishusnan <98398156+harishusnan@users.noreply.github.com>
2022-03-19 17:11:44 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -42,8 +42,8 @@ st.subheader('Class labels and their corresponding index number')
 st.write(["setosa","versicolor","virginica"])
 
 st.subheader('Prediction')
-st.write(["setosa","versicolor","virginica"][prediction])
-#st.write(prediction)
+# st.write(["setosa","versicolor","virginica"][prediction])
+st.write(prediction)
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
